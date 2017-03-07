6f59d2ecc7ccfe99216279542ece9494f964b76f
Eric Jorgensen <eric.jorgensen@mirumagency.com>
2017-03-07 13:29:55 -0600

    Add product action section tests

---
+++ test/e2e/specs/productActionsSecondaryTest.js
@@ -0,0 +1,23 @@
+// For authoring Nightwatch tests, see
+// http://nightwatchjs.org/guide#usage
+
+module.exports = {
+  'productActionsPrimaryTests': function (browser) {
+    // automatically uses dev Server port from /config.index.js
+    // default: http://localhost:8080
+    // see nightwatch.conf.js
+    const devServer = browser.globals.devServerURL
+
+    browser
+      .url(devServer)
+      .waitForElementVisible('#app', 2000)
+      .assert.elementPresent('.product-actions-secondary')
+      .assert.elementPresent('#add-to-registry')
+      .assert.containsText('#add-to-registry', 'Add to Registry')
+      .assert.elementPresent('#add-to-list')
+      .assert.containsText('#add-to-list', 'Add to List')
+      .assert.elementPresent('#share')
+      .assert.containsText('#share', 'Share')
+      .end()
+  }
+}
